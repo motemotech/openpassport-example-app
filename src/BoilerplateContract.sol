94fe2f06fe303a39ac8f954f8a4710308faf43a7
motemotech <i.am.nicoshark@gmail.com>
2024-10-21 15:33:24 +0900

    add variables and verify functions

--- src/BoilerplateContract.sol
+++ src/BoilerplateContract.sol
@@ -3,40 +3,75 @@ pragma solidity ^0.8.18;
 
 import {IOpenPassportVerifier} from "@openpassport-contracts/interfaces/IOpenPassportVerifier.sol";
 import {IGenericVerifier} from "@openpassport-contracts/interfaces/IGenericVerifier.sol";
+import {IBoilerplateContract} from "./IBoilerplateContract.sol";
 import "@openpassport-contracts/libraries/OpenPassportAttributeSelector.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract BoilerplateContract {
+contract BoilerplateContract is ERC721Enumerable, IBoilerplateContract {
     
     IOpenPassportVerifier public openPassportVerifier;
 
-    uint256 public minimumAge;
-    string public nationality;
-    bool public ofac;
-    string[] public excludeCountries;
+    MinimumAge public minimumAge;
+    Nationality public nationality;
+    Ofac public ofac;
+    ExcludeCountries public excludeCountries;
 
-    constructor(address _openPassportVerifier) {
+    constructor(
+        address _openPassportVerifier,
+        MinimumAge memory _minimumAge,
+        Nationality memory _nationality,
+        Ofac memory _ofac,
+        ExcludeCountries memory _excludeCountries
+    ) ERC721("OpenPassport", "OpenPassport") {
         openPassportVerifier = IOpenPassportVerifier(_openPassportVerifier);
-    }
-
-    function setMinimumAge(uint256 _minimumAge) public {
-        if (_minimumAge < 10) {
-            revert("Minimum age must be at least 10 years old");
-        } else if (_minimumAge > 100) {
-            revert("Minimum age must be at most 100 years old");
-        }
         minimumAge = _minimumAge;
-    }
-
-    function setNationality(string memory _nationality) public {
         nationality = _nationality;
-    }
-
-    function setExcludeCountries(string[] memory _excludeCountries) public {
+        ofac = _ofac;
         excludeCountries = _excludeCountries;
     }
 
-    function setOfac(bool _ofac) public {
-        ofac = _ofac;
+    function verify(
+        uint256 proveVerifierId,
+        uint256 dscVerifierId,
+        IGenericVerifier.ProveCircuitProof memory pProof,
+        IGenericVerifier.DscCircuitProof memory dProof
+    ) public returns (IOpenPassportVerifier.PassportAttributes memory) {
+        uint256[] memory selectors = new uint256[](4);
+        uint256  index = 0;
+
+        if (minimumAge.enabled) {
+         selectors[index++] = OpenPassportAttributeSelector.OLDER_THAN_SELECTOR;
+        }
+        if (nationality.enabled) {
+            selectors[index++] = OpenPassportAttributeSelector.NATIONALITY_SELECTOR;
+        }
+        if (ofac.enabled) {
+            selectors[index++] = OpenPassportAttributeSelector.OFAC_RESULT_SELECTOR;
+        }
+        if (excludeCountries.enabled) {
+            selectors[index++] = OpenPassportAttributeSelector.FORBIDDEN_COUNTRIES_SELECTOR;
+        }
+
+        uint256 combinedSelector = OpenPassportAttributeSelector.combineAttributeSelectors(selectors);
+        
+        IOpenPassportVerifier.PassportAttributes memory passportAttributes = openPassportVerifier.verifyAndDiscloseAttributes(
+            proveVerifierId, 
+            dscVerifierId, 
+            pProof, 
+            dProof,
+            combinedSelector
+        );
+
+        return passportAttributes;
+    }
+
+    function mint(
+        uint256 proveVerifierId,
+        uint256 dscVerifierId,
+        IGenericVerifier.ProveCircuitProof memory pProof,
+        IGenericVerifier.DscCircuitProof memory dProof
+    ) public {
+
     }
 
 }
